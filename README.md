f48c268eebdd96a6205861f878d16267aac29a48
Ivan Porto Carrero <ivan@flanders.co.nz>
2016-02-22 23:33:03 -0800

    print to provided stream

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ A golang HTTP middleware that dumps request as curl commands.
 When the request has a json body then it will also dump the json.
 
 ```go
-curler.New(handler)
+import (
+  "os"
+  "github.com/casualjim/curler"
+)
+
+curler.New(handler, os.Stdout)
 ```
--- curler.go
+++ curler.go
@@ -3,14 +3,14 @@ package curler
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strings"
 )
 
 // New creates a new curler
-func New(orig http.Handler) http.Handler {
+func New(orig http.Handler, out io.Writer) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		parts := []string{"curl", "-v", "-X", r.Method}
 
@@ -25,7 +25,7 @@ func New(orig http.Handler) http.Handler {
 			b, err := ioutil.ReadAll(r.Body)
 
 			if err != nil {
-				log.Println("curler:", err)
+				fmt.Fprintln(out, "curler:", err)
 			}
 			if err == nil && len(b) > 0 {
 				r.Body = ioutil.NopCloser(bytes.NewBuffer(b))
@@ -34,7 +34,7 @@ func New(orig http.Handler) http.Handler {
 		}
 
 		parts = append(parts, r.URL.String())
-		fmt.Println(strings.Join(parts, " "))
+		fmt.Fprintln(out, strings.Join(parts, " "))
 		orig.ServeHTTP(rw, r)
 	})
 }
